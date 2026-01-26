bfbdb62aeb114193db64933fc94ccb2cda4cea76
Jessica Jain <63677887+jessicajain111@users.noreply.github.com>
2026-01-25 19:22:43 -0500

    Update GitHub link to point to new repository

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -282,7 +282,7 @@ export default function Lab1() {
         to get dummy text<br/>
         
         Please
-        <a href="https://github.com/jessicajain111" id="wd-github">click here</a>
+        <a href="https://github.com/jessicajain111/kambaz-next-js" id="wd-github">click here</a>
         to see my github<br/>
 
     </div>
