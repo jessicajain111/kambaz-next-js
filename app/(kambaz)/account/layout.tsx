b9df66b486910f5944715bf5d4e51678e345341d
Jessica Jain <63677887+jessicajain111@users.noreply.github.com>
2026-01-25 19:38:12 -0500

    Rename KambazLayout to AccountLayout and update structure

--- app/(kambaz)/account/layout.tsx
+++ app/(kambaz)/account/layout.tsx
@@ -1,21 +1,23 @@
 import { ReactNode } from "react";
-import KambazNavigation from "./Navigation";
+import AccountNavigation from "./Navigation";
 
-export default function KambazLayout({
+export default function AccountLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td valign="top" width="200">
-            <KambazNavigation />
-          </td>
-          <td valign="top" width="100%">
-            {children}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="wd-kambaz">
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top">
+              <AccountNavigation />
+            </td>
+            <td valign="top" width="100%">
+              {children}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
