342f92efdac2a444e6f72b5e7af1dba9590e0f23
jessicajain111 <jessicajain111@gmail.com>
2026-01-25 20:44:37 -0500

    done

--- app/(kambaz)/Navigation.tsx
+++ app/(kambaz)/Navigation.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-
 export default function KambazNavigation() {
   return (
     <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">
-        Northeastern</a><br/>
+      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
       <Link href="/account" id="wd-account-link">Account</Link><br/>
       <Link href="/dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
       <Link href="/dashboard" id="wd-course-link">Courses</Link><br/>
@@ -12,5 +10,4 @@ export default function KambazNavigation() {
       <Link href="/inbox" id="wd-inbox-link">Inbox</Link><br/>
       <Link href="/labs" id="wd-labs-link">Labs</Link><br/>
     </div>
-  );
-}
+);}
