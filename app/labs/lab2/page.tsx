19748f526f44f84005475179ada33da3f97aa96c
jessicajain111 <jessicajain111@gmail.com>
2026-01-27 12:56:08 -0500

    issues

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -1,24 +1,24 @@
 import "./index.css";
 import { Container } from "react-bootstrap";
-import ForegroundColors from "./ForegroundColors";
-import BackgroundColors from "./BackgroundColors";
-import Padding from "./Padding";
-import Margins from "./Margins";
-import Corners from "./Corners";
-import Dimensions from "./Dimensions";
-import Positions from "./Positions";
-import Zindex from "./Zindex";
-import Float from "./Float";
-import GridLayout from "./GridLayout";
-import Flex from "./Flex";
-import MediaQueriesDemo from "./MediaQueriesDemo";
-import ReactIconsSampler from "./ReactIconsSampler";
-import BootstrapGrids from "./BootstrapGrids";
-import ScreenSizeLabel from "./ScreenSizeLabel";
-import BootstrapTables from "./BootstrapTables";
-import BootstrapLists from "./BootstrapLists";
-import BootstrapForms from "./BootstrapForms";
-import BootstrapNavigation from "./BootstrapNavigation";
+// import ForegroundColors from "./ForegroundColors";
+// import BackgroundColors from "./BackgroundColors";
+// import Padding from "./Padding";
+// import Margins from "./Margins";
+// import Corners from "./Corners";
+// import Dimensions from "./Dimensions";
+// import Positions from "./Positions";
+// import Zindex from "./Zindex";
+// import Float from "./Float";
+// import GridLayout from "./GridLayout";
+// import Flex from "./Flex";
+// import MediaQueriesDemo from "./MediaQueriesDemo";
+// import ReactIconsSampler from "./ReactIconsSampler";
+// import BootstrapGrids from "./BootstrapGrids";
+// import ScreenSizeLabel from "./ScreenSizeLabel";
+// import BootstrapTables from "./BootstrapTables";
+// import BootstrapLists from "./BootstrapLists";
+// import BootstrapForms from "./BootstrapForms";
+// import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
@@ -72,7 +72,7 @@ export default function Lab2() {
         </div>
       </div>
 
-      <ForegroundColors />
+      {/* <ForegroundColors />
       <BackgroundColors />
       <Padding />
       <Margins />
@@ -90,7 +90,7 @@ export default function Lab2() {
       <BootstrapTables />
       <BootstrapLists />
       <BootstrapForms />
-      <BootstrapNavigation />
+      <BootstrapNavigation /> */}
     </Container>
   );
 }
