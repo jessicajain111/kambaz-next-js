0817c495c591ffc653cea73876c2e85be5be1cd8
jessicajain111 <jessicajain111@gmail.com>
2026-02-06 09:19:26 -0500

    adding final touches 3

--- app/(kambaz)/courses/[cid]/assignments/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/page.tsx
@@ -4,6 +4,7 @@ import { BsSearch, BsPlusLg, BsGripVertical } from "react-icons/bs";
 import { FaCheckCircle } from "react-icons/fa";
 import { IoEllipsisVertical } from "react-icons/io5";
 import { MdOutlineAssignment } from "react-icons/md";
+import Link from "next/link";
 
 export default function Assignments() {
   return (
@@ -56,7 +57,9 @@ export default function Assignments() {
                 <BsGripVertical className="me-3 fs-4 text-muted" />
                 <MdOutlineAssignment className="me-3 fs-3 text-success" />
                 <div className="flex-grow-1">
-                  <div className="fw-bold">A1</div>
+                  <Link href="/courses/1234/assignments/A1" className="fw-bold text-decoration-none text-dark">
+                    A1
+                  </Link>
                   <div className="text-danger small">
                     Multiple Modules | <span className="text-muted">Not available until</span> May 6 at 12:00am | <span className="text-muted">Due</span> May 13 at 11:59pm | 100 pts
                   </div>
@@ -74,7 +77,9 @@ export default function Assignments() {
                 <BsGripVertical className="me-3 fs-4 text-muted" />
                 <MdOutlineAssignment className="me-3 fs-3 text-success" />
                 <div className="flex-grow-1">
-                  <div className="fw-bold">A2</div>
+                  <Link href="/courses/1234/assignments/A2" className="fw-bold text-decoration-none text-dark">
+                    A2
+                  </Link>
                   <div className="text-danger small">
                     Multiple Modules | <span className="text-muted">Not available until</span> May 13 at 12:00am | <span className="text-muted">Due</span> May 20 at 11:59pm | 100 pts
                   </div>
@@ -92,7 +97,9 @@ export default function Assignments() {
                 <BsGripVertical className="me-3 fs-4 text-muted" />
                 <MdOutlineAssignment className="me-3 fs-3 text-success" />
                 <div className="flex-grow-1">
-                  <div className="fw-bold">A3</div>
+                  <Link href="/courses/1234/assignments/A3" className="fw-bold text-decoration-none text-dark">
+                    A3
+                  </Link>
                   <div className="text-danger small">
                     Multiple Modules | <span className="text-muted">Not available until</span> May 20 at 12:00am | <span className="text-muted">Due</span> May 27 at 11:59pm | 100 pts
                   </div>
