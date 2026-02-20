0192d690703066fa4143a68c89b9530288cec371
jessicajain111 <jessicajain111@gmail.com>
2026-02-20 17:01:18 -0500

    fix2

--- app/(kambaz)/courses/[cid]/people/page.tsx
+++ app/(kambaz)/courses/[cid]/people/page.tsx
@@ -1,5 +1,51 @@
-import PeopleTable from "./Table/page";
+"use client";
+
+import { Table } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+import { useParams } from "next/navigation";
+import * as db from "../../../database";
 
 export default function People() {
-  return <PeopleTable />;
+  const { cid } = useParams();
+  const { users, enrollments } = db as any;
+
+  return (
+    <div id="wd-people-table">
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(users as any[])
+            .filter((usr) =>
+              (enrollments as any[]).some(
+                (enrollment) =>
+                  enrollment.user === usr._id && enrollment.course === cid
+              )
+            )
+            .map((user: any) => (
+              <tr key={user._id}>
+                <td className="wd-full-name text-nowrap">
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">{user.firstName}</span>{" "}
+                  <span className="wd-last-name">{user.lastName}</span>
+                </td>
+                <td className="wd-login-id">{user.loginId}</td>
+                <td className="wd-section">{user.section}</td>
+                <td className="wd-role">{user.role}</td>
+                <td className="wd-last-activity">{user.lastActivity}</td>
+                <td className="wd-total-activity">{user.totalActivity}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
